adbef497fd1dffce3c026d5d6fc930890d694d63
sinju <sinju@DESKTOP-HOO85PR>
2024-05-23 16:31:18 +0530

    framework commit

---
+++ SeleniumFramework/src/main/java/base/DriverIntializer.java
@@ -0,0 +1,65 @@
+package base;
+import java.io.IOException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import util.PropertyReaderUtil;
+
+	public class DriverIntializer {
+		public WebDriver driver; 
+		//public static final String URL="https://selenium.qabible.in/index.php";
+		
+		 @Parameters({"browser"})
+		 @BeforeClass
+		public void setup(@Optional("Edge")String browser) throws IOException{
+			driver = createWebDriverSession(browser);
+			driver.get(PropertyReaderUtil.getproperty("URL"));
+			
+		}
+		
+		 public WebDriver getdriver(){
+			System.out.println(driver);
+			return this.driver;
+			
+		}
+		
+		 public WebDriver createWebDriverSession(String browser) {
+			// TODO Auto-generated method stub
+			
+			switch(browser){
+			case "Chrome":
+			   System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe");
+			   driver = new ChromeDriver();
+			   driver.manage().window().maximize();
+			   break;
+			case "Edge":
+				System.setProperty("webdriver.edge.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\msedgedriver.exe");
+				driver = new EdgeDriver();
+				driver.manage().window().maximize();
+			    break;
+			case "Firefox":
+				System.setProperty("webdriver.firefox.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\geckodriver.exe");
+				driver = new FirefoxDriver();
+				driver.manage().window().maximize();
+			    break;
+			case "default":
+				System.out.println("nothing");
+				
+			}
+			return driver;
+		}
+		 @AfterClass
+		 public void browserclose() {
+			 driver.quit();		 
+		  }
+		
+		 
+	}
+
+
---
+++ SeleniumFramework/src/main/java/pages/ProgressBarPages.java
@@ -0,0 +1,19 @@
+package pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ProgressBarPages {
+	WebDriver driver ;
+	By JQuery=By.xpath("//a[text()='JQuery UI Progress']");
+	
+	public ProgressBarPages (WebDriver driver )
+	{
+		this. driver = driver;
+	}
+	
+	public void JQueryclick()
+	{
+		driver.findElement(JQuery).click();
+	}
+
+}
