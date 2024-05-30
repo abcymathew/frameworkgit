9c6e09d3736f48287c4cba921774434a47124a83
sinju <sinju@DESKTOP-HOO85PR>
2024-05-30 20:29:55 +0530

    chrome commit

--- SeleniumFramework/src/main/java/base/DriverIntializer.java
+++ SeleniumFramework/src/main/java/base/DriverIntializer.java
@@ -35,9 +35,9 @@ import util.PropertyReaderUtil;
 			
 			switch(browser){
 			case "Chrome":
-			WebDriverManager.chromedriver().setup();
-			  //System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe");
-			   //System.out.println("chromepath"+System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe");
+			//WebDriverManager.chromedriver().setup();
+			  System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe");
+			   System.out.println("chromepath"+System.getProperty("user.dir")+"\\src\\main\\resources\\chromedriver.exe");
 					   driver = new ChromeDriver();
 			   driver.manage().window().maximize();
 			   break;
